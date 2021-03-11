bb11618ff504f3b5140ca39f39551e837ee10d58
Ilia-tod29 <61384294+Ilia-tod29@users.noreply.github.com>
2021-03-11 14:17:50 +0200

    Update Week2_Task16.hs

--- Week2HW/Week2_Task16.hs
+++ Week2HW/Week2_Task16.hs
@@ -4,12 +4,12 @@ main = do
     print $ reverseOrdSuff 32763 == 367
     print $ reverseOrdSuff 32567 == 7
     print $ reverseOrdSuff 32666 == 6
+    print $ reverseOrdSuff 4321
 
 reverseOrdSuff :: Int -> Int
 reverseOrdSuff num = helper (div num 10) (mod num 10)
  where
      helper :: Int -> Int -> Int
      helper thisNum result
-      | div thisNum 10 < 10 = result
       | mod thisNum 10 > mod result 10 = helper (div thisNum 10) ((result * 10) + mod thisNum 10)
-      | otherwise = helper (div thisNum 10) result
+      | otherwise = result

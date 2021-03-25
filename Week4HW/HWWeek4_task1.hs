7126e740ba43966b07e1be42dea1993230290e30
Ilia Todorov <61384294+Ilia-tod29@users.noreply.github.com>
2021-03-25 23:03:56 +0200

    Update HWWeek4_task1.hs

--- Week4HW/HWWeek4_task1.hs
+++ Week4HW/HWWeek4_task1.hs
@@ -8,11 +8,12 @@ main = do
     print $ mySumFunc [1, 2, 3] == 6
 
 mySumRecNonPM :: [Double] -> Double
-mySumRecNonPM xs = sum xs
+mySumRecNonPM [] = 0
+mySumRecNonPM xs = head xs + mySumRecNonPM (tail xs)
 
 mySumRecPM :: [Double] -> Double
 mySumRecPM [] = 0
-mySumRecPM xs = sum xs
+mySumRecPM (x:xs) = x + mySumRecPM xs
 
 mySumFunc :: [Double] -> Double
 mySumFunc xs = sum $ (map (\ x -> x) xs)
